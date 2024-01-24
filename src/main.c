d0800ac402b5ab71714d3bfb448d0e3b68379c82
elloramir <cripboygit@gmail.com>
2024-01-24 15:24:55 -0300

    memory arenas

--- src/main.c
+++ src/main.c
@@ -2,9 +2,12 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "os_opengl.h"
 #include "os_api.h"
+
+#include "arena.c"
 
 #if defined(_WIN32) || defined(_WIN64)
 #include "os_win32.c"
@@ -12,6 +15,18 @@
 
 int entry_point ( void ) {
 	os_create_window(800, 600, "hello sailor");
+
+#define PRINT_ARENA(arena) printf("Arena: size=%zu offset=%zu\n", arena.size, arena.offset)
+
+	// Lets test arena
+	Arena arena;
+	arena_init(&arena, 4096);
+	PRINT_ARENA(arena);
+	const char *str = arena_sprintf(&arena, "Hello %s", "World");
+	printf("alocates: %s\n", str);
+	PRINT_ARENA(arena);
+	arena_reset(&arena);
+	PRINT_ARENA(arena);
 
 	// TODO(ellora): Move to logging system
 	printf("[LOG] %s\n", glGetString(GL_VERSION));
